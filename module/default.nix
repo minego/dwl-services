cbf0c3a37a18ad50e1c407c07d1f7b79c2422393
Micah N Gorrell <m@minego.net>
2024-03-11 17:29:32 -0600

    Updates

--- module/default.nix
+++ module/default.nix
@@ -8,9 +8,6 @@ with lib;
 			serviceConfig = {
 				Type				= "notify";
 				ExecStart			= "${pkgs.dwl}/bin/dwl";
-				Restart				= "on-failure";
-				RestartSec			= 1;
-				TimeoutStopSec		= 10;
 				NotifyAccess		= "all";
 				ExecStopPost		= "/usr/bin/systemctl --user unset-environment DISPLAY WAYLAND_DISPLAY";
 			};
